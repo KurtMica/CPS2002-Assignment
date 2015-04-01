cc28fdc6c9259e3808c608af2ba7d008934c45d7
Morerice <jeant.m24@gmail.com>
2015-04-01 14:27:41 +0200

    Add testAccountBalancePositive (test) to AccountTest
    Both classes were renamed to Account and AccountTest respectively.
    Constructor and private variables were added to Account.
    The first test specified above was added to AccountTest

---
+++ src/main/java/mt/edu/um/kurtjeantwan/Account.java
@@ -0,0 +1,20 @@
+package mt.edu.um.kurtjeantwan;
+
+/**
+ * Shell code used for setup checks.
+ *
+ */
+public class Account 
+{
+	private int accountNumber;
+        private String accountName;
+        private long accountBalance;
+        
+        public Account(int num, String Name)
+        {
+            accountNumber = num;
+            accountName = Name;
+            accountBalance = 0;
+        
+        }
+}
---
+++ src/test/java/mt/edu/um/kurtjeantwan/AccountTest.java
@@ -0,0 +1,26 @@
+package mt.edu.um.kurtjeantwan;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
+
+/**
+ * Test for shell code.
+ */
+public class AccountTest 
+{
+    private Account acc1;
+    
+    @Before
+    public void setup()
+    {
+        acc1 = new Account(1, "First");   
+    }
+    
+    @Test
+    public void testAccountBalancePositive()
+    {
+    	Assert.assertEquals(true, acc1.accountBalance(5));
+    }
+    
+}

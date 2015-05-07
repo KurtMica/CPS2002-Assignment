e9642836ceb55d75b2b0f3245e1304238ff37de9
Morerice <jeant.m24@gmail.com>
2015-05-07 23:43:50 +0200

    Change methods to MainTransaction

--- src/main/java/mt/edu/um/kurtjeantwan/MainTransaction.java
+++ src/main/java/mt/edu/um/kurtjeantwan/MainTransaction.java
@@ -5,22 +5,68 @@ import java.util.List;
 
 public class MainTransaction extends CompoundTransaction {
     
-    private AccountDatabase accountdB;
+    private AccountDatabase accountDb;
     private Risk risk;
     private Commision commision;
     
     public MainTransaction(AccountDatabase db, Risk risk)
     {
-         
+         super("Main Transaction");
+         this.accountDb = db;
+         this.risk = risk;
+         this.commision = new Commision(this.accountDb, this.risk);
     
     }
     
-    public boolean addTransactions(List<Integer> destinations, List<Double> amounts)
+    public boolean addTransactions(List<Integer> destinations, List<Double> amounts) throws Exception
     {
     
-        return false;
+        if(destinations.size() != amounts.size())
+        {
+            return false;
+        
+        }
+        
+        double sum = 0;
+        
+        
+        for(int i =0; i<destinations.size(); i++)
+        {
+            
+            CompoundTransaction temp = new CompoundTransaction();
+            temp.addTransaction(new Deposit(destinations.get(i), 0.20 * amounts.get(i), this.accountDb, this.risk));
+            temp.addTransaction(new AtomicTransaction(getSource(), destinations.get(i), amounts.get(i) * 0.8, this.accountDb));
+            
+            this.subTransactions.add(temp);
+            sum += amounts.get(i);
+        
+        
+        }
+        this.commision.addCommision(sum);
+        this.commision.process();
+        return true;
     
     }
+    
+    
+    public int getSource()
+    {
+        
+        if(this.risk == Risk.high){
+        
+            return 3143;
+        
+        } else {
+        
+            return 3133;
+        }
+    
+    
+    
+    }
+    
+    
+    
     
     
     
